41cef8401e12b70a02de65e8335f20a963ba32b2
David RAFFEIL <r2david2@hotmail.fr>
2025-06-01 14:20:29 +0200

    Suppression complète de la V1.2 pour nettoyage

--- versions_archivées/v1.2/test_defillama.py
+++
@@ -1,8 +0,0 @@
-# test_defillama.py
-
-from dex_defillama import get_pools_defillama
-
-pools = get_pools_defillama()
-
-for i, pool in enumerate(pools):
-    print(f"#{i+1} - {pool['dex']} - {pool['token0']}/{pool['token1']} - TVL: ${pool['tvl_usd']:.2f} - APR: {pool['apr']:.2f}%")
